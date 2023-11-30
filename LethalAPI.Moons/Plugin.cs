73ec3bebd2839ae471c0f6d9b1c4899ce2bc9169
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-30 01:33:15 -0600

    fix nuget config and description

--- LethalAPI.Moons/Plugin.cs
+++ LethalAPI.Moons/Plugin.cs
@@ -37,7 +37,7 @@ public class Plugin : LethalAPI.Core.Features.Plugin<Config>
     public override string Name => "LethalAPI.Moons";
 
     /// <inheritdoc />
-    public override string Description => "PROJECT-DESCRIPTION";
+    public override string Description => "Moon-related functionality for LethalAPI";
 
     /// <inheritdoc />
     public override string Author => "LethalAPI Modding Community";
